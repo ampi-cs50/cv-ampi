9118f68182acf01af2a4b02c4ce18a79aeceda39
ampi-cs50 <152340325+ampi-cs50@users.noreply.github.com>
2026-05-02 16:57:21 -0300

    Se borraron los cambios no relacionados con el boton

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
+const boton = document.getElementById("scrollToTop");
 
-const boton = document.getElementById('scrollToTop');
-
-
-window.onscroll = function() {
-    if (boton){
-        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-            boton.style.display = "block";
-        } else {
-            boton.style.display = "none";
-        }
+window.onscroll = function () {
+  if (boton) {
+    if (
+      document.body.scrollTop > 100 ||
+      document.documentElement.scrollTop > 100
+    ) {
+      boton.style.display = "block";
+    } else {
+      boton.style.display = "none";
     }
+  }
 };
 
-if (boton){
-    boton.addEventListener('click', () => {
-        window.scrollTo({ top: 0, behavior: 'smooth'});
-    });
+if (boton) {
+  boton.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
 }
