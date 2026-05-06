bf62a66abaff598f0d52fdea4bfafbd5f78aef90
ampi-cs50 <152340325+ampi-cs50@users.noreply.github.com>
2026-05-06 10:31:57 -0300

    restaura git commit -m, restaura boton con formato correcto y git push origin 22-botonScrollToTop del codigo con formato correcto

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-// Requisito C: Funcionalidad de botón visible
-const boton = document.getElementById("btnIrArriba");
 
-// Mostrar el botón cuando el usuario baja en la página
-window.onscroll = function () {
-  if (
-    document.body.scrollTop > 100 ||
-    document.documentElement.scrollTop > 100
-  ) {
-    boton.style.display = "block";
-  } else {
-    boton.style.display = "none";
-  }
+const boton = document.getElementById('scrollToTop');
+
+
+window.onscroll = function() {
+    if (boton){
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            boton.style.display = "block";
+        } else {
+            boton.style.display = "none";
+        }
+    }
 };
 
-// Acción de volver arriba al presionar
-boton.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-});
+if (boton){
+    boton.addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth'});
+    });
+}
